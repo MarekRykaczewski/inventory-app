0d0de9ffa9ace88abd08e596f6df638f16b1a694
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-01 12:28:19 +0200

    add catalog list

---
+++ controllers/categoryController.js
@@ -0,0 +1,12 @@
+const Category = require('../models/category')
+const asyncHandler = require('express-async-handler')
+
+// Display list of all BookInstances.
+exports.category_list = asyncHandler(async (req, res, next) => {
+    const allCategories = await Category.find().sort({ name: 1 }).exec();
+  
+    res.render("category_list", {
+      title: "Category list",
+      category_list: allCategories,
+    });
+  });
--- routes/catalog.js
+++ routes/catalog.js
@@ -4,10 +4,16 @@ const router = express.Router();
 // Require controller modules
 
 const item_controller = require('../controllers/itemController')
+const category_controller = require('../controllers/categoryController')
 
 // ITEM ROUTES
 
 // GET catalog item_list
 router.get("/", item_controller.item_list);
+
+// CATEGORY ROUTES
+router.get("/categories", category_controller.category_list);
+
+// Export
 
 module.exports = router;
